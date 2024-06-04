0499ae55b12054c145be67bcd9b1076171f98d8b
ph delville <phdelville33@gmail.com>
2024-06-04 09:30:01 +0200

    Ajout d'une size et d'un bool pour passer en gris ou non

--- app/preprocess_bw_resized.py
+++ app/preprocess_bw_resized.py
@@ -2,12 +2,15 @@
 import numpy as np
 import cv2
 
-def proc_to_bw_resized(img : np.ndarray )-> np.ndarray :
+def proc_to_bw_resized(img : np.ndarray , new_shape : tuple = (128,128), gray : bool = True)-> np.ndarray :
     """
-    Process an image from cv2.imread(img_path) : convert to black and white
+    Process an image from cv2.imread(img_path, new_shape, gray : bool ) : convert to black and white
     and resize to (64,64)
     """
     img_cv2 = img[...,::-1] # Convert BGR to RGB
-    res = cv2.resize(img_cv2, dsize=(64, 64))     # Resizing
-    gray_image = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY) # Convert to Grayscale
-    return gray_image
+    res = cv2.resize(img_cv2, dsize=new_shape)     # Resizing
+    if gray == True:
+        gray_image = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY) # Convert to Grayscale
+        return gray_image
+    else:
+        return res
